bc32d5713f806d363b2b559c4514d4d20eeb08e7
Brandon Milton <brandonio21@linux.com>
2015-05-04 19:37:12 -0700

    Added a C++ template by default

---
+++ Solutions/Templates/Problem1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Parameter(s): int number - The decimal number to be converted to binary
+// Return:       int        - The binary version of the provided decimal number
+int convertToBinary(int number) {
+  // YOUR CODE HERE
+  return 0;
+}
+
+///////////////////////////// DO NOT MODIFY ANYTHING BELOW THIS LINE ///////////
+int main() {
+  int num;
+  while (cin >> num) {
+    cout << convertToBinary(num) << endl;
+  }
+}
